5b2f555aefb15e6856f65f2d06da2e14fbe4e79f
nubal.ex <nicolas.ubal.ex@consalud.cl>
2025-08-21 22:22:55 -0400

    agrego carpeta repositorio

---
+++ src/repositories/url.repositories.ts
@@ -0,0 +1,21 @@
+﻿import prisma from './prisma';
+import { Supermercado } from '@/generated/prisma';
+
+export const findAllUrls = async (): Promise<Supermercado[]> => {
+    return await prisma.supermercado.findMany({
+        orderBy: { createdAt: 'desc' },
+    });
+};
+
+export const findUrlByAddress = async (address: string): Promise<Supermercado | null> => {
+    return await prisma.supermercado.findUnique({
+        where: { name: address },
+    });
+};
+
+export const createUrl = async (address: string): Promise<Supermercado> => {
+
+    return await prisma.supermercado.create({
+        data: { name: address },
+    });
+};
